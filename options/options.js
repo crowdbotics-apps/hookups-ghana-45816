d80275dbfac99595140f4a5fd4c828441cfe9b12
Crowdbotics <team@crowdbotics.com>
2024-02-03 10:10:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hookups-ghana-45816",
+  url: "https://hookups-ghana-45816.botics.co",
+  api: "https://hookups-ghana-45816.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
